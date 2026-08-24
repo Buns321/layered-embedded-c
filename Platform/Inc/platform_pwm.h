27e153eaf8012cb39f89c11580cfec1d43e9d743
Buns321 <buns_music@outlook.com>
2026-08-24 22:47:32 +0800

    Added Platform_PWM_SetDuty function

--- Platform/Inc/platform_pwm.h
+++ Platform/Inc/platform_pwm.h
@@ -9,9 +9,9 @@
 #include "platform_tim.h"
 #include <stdint.h>
 
-
 Platform_StatusTypeDef Platform_PWM_Start(Platform_TIM_Instance tim, Platform_TIM_Channel channel);
 Platform_StatusTypeDef Platform_PWM_Stop(Platform_TIM_Instance tim, Platform_TIM_Channel channel);
 void Platform_PWM_SetCompare(Platform_TIM_Instance tim, Platform_TIM_Channel channel, uint32_t compare);
+void Platform_PWM_SetDuty(Platform_TIM_Instance tim, Platform_TIM_Channel channel, uint16_t max_duty, uint16_t duty);
 
 #endif //LAYERED_EMBEDDED_C_PLATFORM_PWM_H
